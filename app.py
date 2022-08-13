0fc35d8083a838c6e815cf155ff4b6217b2a30f2
Lucsakai <84025800+Lucsakai@users.noreply.github.com>
2022-08-13 11:57:40 -0300

    Commit 2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World"
+    return "Mão na massa, Aula DevOps_Impacta! --Lucas Sakai--"
 
 if __name__ == '__main__':
     app.run()
